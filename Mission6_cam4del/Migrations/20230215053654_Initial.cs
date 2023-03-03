1b0097c9bc5f0d657cba3220ec58901288ef8700
Cameron Delgrosso <cam4del@byu.edu>
2023-03-03 14:28:36 -0700

    Mission 7

---
+++ Mission6_cam4del/Migrations/20230215053654_Initial.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission6_cam4del.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "responses",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(nullable: false),
+                    year = table.Column<int>(nullable: false),
+                    director = table.Column<string>(nullable: false),
+                    rating = table.Column<string>(nullable: false),
+                    edited = table.Column<bool>(nullable: false),
+                    lent_to = table.Column<string>(nullable: true),
+                    notes = table.Column<string>(maxLength: 25, nullable: true),
+                    CategoryID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_responses", x => x.MovieId);
+                    table.ForeignKey(
+                        name: "FK_responses_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 1, "Action" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 2, "Comedy" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 3, "Horror" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 4, "Thriller" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 5, "Romance" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 6, "Animation" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName" },
+                values: new object[] { 7, "Documentary" });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "MovieId", "CategoryID", "director", "edited", "lent_to", "notes", "rating", "title", "year" },
+                values: new object[] { 1, 1, "John Singleton", false, "Cameron Delgrosso", "Very fast, not very furious", "PG-13", "2 Fast 2 Furious", 2003 });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "MovieId", "CategoryID", "director", "edited", "lent_to", "notes", "rating", "title", "year" },
+                values: new object[] { 3, 1, "Joseph Kosinski", false, "Cameron Delgrosso", "Gave me chills", "PG-13", "Top Gun Maverick", 2022 });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "MovieId", "CategoryID", "director", "edited", "lent_to", "notes", "rating", "title", "year" },
+                values: new object[] { 2, 2, "Phil Lord", true, "Cameron Delgrosso", "Jonah Hill is funny", "R", "21 Jump Street", 2012 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_responses_CategoryID",
+                table: "responses",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "responses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
--- Mission6_cam4del/Models/ApplicationResponse.cs
+++ Mission6_cam4del/Models/ApplicationResponse.cs
@@ -11,19 +11,23 @@ namespace Mission6_cam4del.Models
         [Key]
         [Required]
         public int MovieId { get; set; }
-        [Required]
-        public string category { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Please enter a movie title")]
         public string title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter a valid year")]
         public int year { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter a director name")]
         public string director { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter a movie rating")]
         public string rating { get; set; }
         public bool edited { get; set; }
         public string lent_to { get; set; }
         [MaxLength(25)]
         public string notes { get; set; }
+
+        //Build foreign key relationship
+        [Required(ErrorMessage = "Please enter a movie category")]
+        public int CategoryID { get; set; }
+        [Required]
+        public Category CategoryName { get; set; }
     }
 }
